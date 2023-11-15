05dd93173e4a253e1ad232f2004e2bc38e48df1a
Mutai05 <kelvok2014@gmail.com>
2023-11-15 12:55:11 -0800

    Created monty.h file to include function prototypes

---
+++ monty.h
@@ -0,0 +1,21 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+/**
+ * struct stack_s - doubly linked list representation of a stack
+ * @n: integer
+ * @prev: points to the previous element of the stack
+ * @next: points to the next element of the stack
+ */
+typedef struct stack_s
+{
+    int n;
+    struct stack_s *prev;
+    struct stack_s *next;
+} stack_t;
+
+/* Function prototypes */
+void push(int value, unsigned int line_number);
+void pall(void);
+
+#endif /* MONTY_H */

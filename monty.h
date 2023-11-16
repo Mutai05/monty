9d1083283c409252e70e2205b5874586104db7ab
Mutai05 <kelvok2014@gmail.com>
2023-11-16 13:50:22 -0800

    Updated monty.h file code

--- monty.h
+++ monty.h
@@ -1,11 +1,22 @@
 #ifndef MONTY_H
 #define MONTY_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/syscall.h>
+#include <string.h>
+
 /**
- * struct stack_s - doubly linked list representation of a stack
- * @n: integer
- * @prev: points to the previous element of the stack
- * @next: points to the next element of the stack
+ * struct stack_s - Represents a node in a doubly linked list for stack or queue
+ * @n: Integer value stored in the node
+ * @prev: Pointer to the previous node in the stack or queue
+ * @next: Pointer to the next node in the stack or queue
+ *
+ * Description: Node structure for a doubly linked list used in stack or queue operations.
  */
 typedef struct stack_s
 {
@@ -14,13 +25,32 @@ typedef struct stack_s
     struct stack_s *next;
 } stack_t;
 
-/* Global variable representing the top of the stack */
-extern stack_t *stack;
+/**
+ * struct instruction_s - Represents an opcode and its associated function
+ * @opcode: The opcode as a string
+ * @f: Pointer to the function that handles the opcode
+ *
+ * Description: Structure defining an opcode and its respective function for stack or queue operations.
+ */
+typedef struct instruction_s
+{
+    char *opcode;
+    void (*f)(stack_t **stack, unsigned int line_num);
+} instruction_t;
 
-/* Function prototypes */
-void push(int value, unsigned int line_number);
-void pall(void);
-void pop(void);
-void swap(void);
+int _strcmp(char *opcode, char *list);
+void add(stack_t **stack, unsigned int line_num);
+void _swap(stack_t **doubly, unsigned int cline);
+void pall(stack_t **stack, unsigned int line_num);
+void execute(char *string[], stack_t *stack);
+void _pint(stack_t **doubly, unsigned int cline);
+void free_stack(stack_t *head);
+void _pop(stack_t **doubly, unsigned int cline);
+int pushint(char *list, int ln);
+void push(stack_t **stack, unsigned int line_number, int n);
+void _pchar(stack_t **doubly, unsigned int cline);
+int nlfind(char *list);
+void free_list(char *a[]);
+int combfind(char *list, int ln);
 
-#endif /* MONTY_H */
+#endif

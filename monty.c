4901ce742cd3e8911271a71e5e75b8f4d5df55c5
Mutai05 <kelvok2014@gmail.com>
2023-11-16 05:05:53 -0800

    Implement the pint opcode.

--- monty.c
+++ monty.c
@@ -5,47 +5,46 @@
 
 stack_t *stack;  /* Global variable representing the top of the stack */
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     FILE *file;
     char opcode[256];
     int value, line_number = 0;
 
-    if (argc != 2)
-    {
+    if (argc != 2) {
         fprintf(stderr, "USAGE: monty file\n");
-        return (EXIT_FAILURE);
+        return EXIT_FAILURE;
     }
 
     file = fopen(argv[1], "r");
 
-    if (file == NULL)
-    {
+    if (file == NULL) {
         fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        return (EXIT_FAILURE);
+        return EXIT_FAILURE;
     }
 
-    while (fscanf(file, "%s", opcode) != EOF)
-    {
+    while (fscanf(file, "%s", opcode) != EOF) {
         line_number++;
 
-        if (strcmp(opcode, "push") == 0)
-        {
+        if (strcmp(opcode, "push") == 0) {
             if (fscanf(file, "%d", &value) == 1)
                 push(value, line_number);
-            else
-            {
+            else {
                 fprintf(stderr, "L%d: usage: push integer\n", line_number);
                 fclose(file);
                 exit(EXIT_FAILURE);
             }
-        }
-        else if (strcmp(opcode, "pall") == 0)
-        {
+        } else if (strcmp(opcode, "pall") == 0) {
             pall();
+        } else if (strcmp(opcode, "pint") == 0) {
+            if (stack == NULL) {
+                fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+                fclose(file);
+                exit(EXIT_FAILURE);
+            }
+            printf("%d\n", stack->n);
         }
     }
 
     fclose(file);
-    return (EXIT_SUCCESS);
+    return EXIT_SUCCESS;
 }

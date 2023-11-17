a9f167694fb9eaa505238f4a147a94efe8b36014
Mutai05 <kelvok2014@gmail.com>
2023-11-16 17:06:41 -0800

    Updated monty.h file

--- monty.h
+++ monty.h
@@ -7,45 +7,52 @@
 #include <fcntl.h>
 #include <string.h>
 #include <ctype.h>
+
 /**
- * struct stack_s - doubly linked list representation of a stack (or queue)
- * @n: integer
- * @prev: points to the previous element of the stack (or queue)
- * @next: points to the next element of the stack (or queue)
+ * struct stack_s - Structure representing a doubly linked list for stacks/queues
+ * @n: Integer value stored in each node
+ * @prev: Pointer to the previous element in the list
+ * @next: Pointer to the next element in the list
  *
- * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO Holberton project
+ * Description: This structure defines a node in a doubly linked list
+ * used for stack and queue operations.
  */
+
 typedef struct stack_s
 {
 	int n;
 	struct stack_s *prev;
 	struct stack_s *next;
 } stack_t;
+
 /**
- * struct bus_s - variables -args, file, line content
- * @arg: value
- * @file: pointer to monty file
- * @content: line content
- * @lifi: flag change stack <-> queue
- * Description: carries values through the program
+ * struct bus_s - Structure holding program data during execution
+ * @arg: Value of an argument
+ * @file: Pointer to the current monty file
+ * @content: Content of a line in the file
+ * @lifi: Flag to switch between stack and queue
+ *
+ * Description: This structure carries essential data throughout
+ * the program's execution.
  */
+
 typedef struct bus_s
 {
 	char *arg;
 	FILE *file;
 	char *content;
 	int lifi;
-}  bus_t;
+} bus_t;
 extern bus_t bus;
+
 /**
- * struct instruction_s - opcode and its function
- * @opcode: the opcode
- * @f: function to handle the opcode
+ * struct instruction_s - Structure representing an operation and its function
+ * @opcode: Operation code
+ * @f: Pointer to the function implementing the operation
  *
- * Description: opcode and its function
- * for stack, queues, LIFO, FIFO Holberton project
+ * Description: This structure associates an operation code with its respective function.
  */
+
 typedef struct instruction_s
 {
 	char *opcode;
@@ -53,26 +60,26 @@ typedef struct instruction_s
 } instruction_t;
 char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
 ssize_t getstdin(char **lineptr, int file);
-char  *clean_line(char *content);
-void f_push(stack_t **head, unsigned int number);
-void f_pall(stack_t **head, unsigned int number);
-void f_pint(stack_t **head, unsigned int number);
+char *clean_line(char *content);
+void function_push(stack_t **head, unsigned int number);
+void function_pall(stack_t **head, unsigned int number);
+void function_swap(stack_t **head, unsigned int counter);
+void function_pint(stack_t **head, unsigned int number);
 int execute(char *content, stack_t **head, unsigned int counter, FILE *file);
-void free_stack(stack_t *head);
-void f_pop(stack_t **head, unsigned int counter);
-void f_swap(stack_t **head, unsigned int counter);
-void f_add(stack_t **head, unsigned int counter);
-void f_nop(stack_t **head, unsigned int counter);
-void f_sub(stack_t **head, unsigned int counter);
-void f_div(stack_t **head, unsigned int counter);
-void f_mul(stack_t **head, unsigned int counter);
-void f_mod(stack_t **head, unsigned int counter);
-void f_pchar(stack_t **head, unsigned int counter);
-void f_pstr(stack_t **head, unsigned int counter);
-void f_rotl(stack_t **head, unsigned int counter);
-void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter);
-void addnode(stack_t **head, int n);
+void f_free_stack(stack_t *head);
+void function_sub(stack_t **head, unsigned int counter);
+void function_div(stack_t **head, unsigned int counter);
+void function_pop(stack_t **head, unsigned int counter);
+void function_mul(stack_t **head, unsigned int counter);
+void function_queue(stack_t **head, unsigned int counter);
+void function_add(stack_t **head, unsigned int counter);
+void function_nop(stack_t **head, unsigned int counter);
+void function_pchar(stack_t **head, unsigned int counter);
+void function_pstr(stack_t **head, unsigned int counter);
+void function_rotl(stack_t **head, unsigned int counter);
+void function_mod(stack_t **head, unsigned int counter);
+void function_rotr(stack_t **head, __attribute__((unused)) unsigned int counter);
+void add_node(stack_t **head, int n);
 void addqueue(stack_t **head, int n);
-void f_queue(stack_t **head, unsigned int counter);
-void f_stack(stack_t **head, unsigned int counter);
+void function_stack(stack_t **head, unsigned int counter);
 #endif
